7478dc9efd609610a35291930c1156c94cbc9c8e
Kyungjik Min <dp.min@lge.com>
2025-06-18 13:37:03 +0900

    install.sh: Copy systemd units
    Signed-off-by: Kyungjik Min <dp.min@lge.com>

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 # This script should be run with 'sudo'
 
 # Prerequisites
-# - copy {dump1090,adsbhub}.service at /lib/systemd/system/
+# - internet connection
 
 wget https://www.adsbhub.org/downloads/adsbhub.php --no-check-certificate -O /usr/bin/adsbhub.sh
 chmod 755 /usr/bin/adsbhub.sh
+
+cp ./systemd-units/*.service /lib/systemd/system/
 
 systemctl enable dump1090.service
 systemctl enable adsbhub.service
